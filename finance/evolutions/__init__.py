6d05be29d7a5680534b3901c655e89c64ac86a78
Jeff Larson <thejefflarson@gmail.com>
2009-02-23 06:45:36 +0000

    added template caching to the industry page, it's a bandaid.

--- finance/evolutions/__init__.py
+++ finance/evolutions/__init__.py
@@ -1,1 +1,1 @@
-SEQUENCE = ['add_approval_fields_to_donations', 'add_approval_fields_to_donors', 'add_mtime']
+SEQUENCE = ['add_approval_fields_to_donations', 'add_approval_fields_to_donors', 'add_mtime', 'delete_zips']
